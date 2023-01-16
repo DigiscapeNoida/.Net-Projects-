0ea2f56e99c4486f86ad74bebd8ac86cf52a64ee
dotnet2023 <122510427+dotnet2023@users.noreply.github.com>
2023-01-16 10:47:22 +0530

    Add files via upload

---
+++ Comp Mail Read/ArticleAontentReport/ACRDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArticleAontentReport
+{
+    class ACRDetail
+    {
+        public string SNO
+        { get; set; }
+
+        public string AID
+        { get; set; }
+
+        public string CorAuthor
+        { get; set; }
+
+        public string CorEmail
+        { get; set; }
+
+        public ACRDetail()
+        { }
+    }
+}
---
+++ Comp Mail Read/ExcelRead==/ExcelRead/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.ComponentModel;
+using System.Data;
+using System;
+using System.Xml;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcelRead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ArticleAontentReport.ProcessACRExcel ACRExcelOBJ = new ArticleAontentReport.ProcessACRExcel(@"D:\JWUSA\ArticleContentReport");
+            ACRExcelOBJ.StartProcess();
+        }
+    }
+
+}
